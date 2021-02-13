d65d1be83f6e14d6d30090d82dd419733df95676
Frederic Vogels <frederic.vogels@ucll.be>
2021-02-13 14:44:13 +0100

    Gemfile

---
+++ lib/binstruct.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "binstruct/version"
+
+module Binstruct
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/binstruct/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Binstruct
+  VERSION = "0.1.0"
+end
